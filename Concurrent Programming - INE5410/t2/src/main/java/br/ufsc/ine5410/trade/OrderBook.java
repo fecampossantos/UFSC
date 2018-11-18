30fabb1a92468b362b435567240169be83e78306
Felipe de Campos Santos <36992818+felipecampossantos@users.noreply.github.com>
2018-11-18 12:01:31 -0200

    Add files via upload

--- Concurrent Programming - INE5410/t2/src/main/java/br/ufsc/ine5410/trade/OrderBook.java
+++ Concurrent Programming - INE5410/t2/src/main/java/br/ufsc/ine5410/trade/OrderBook.java
@@ -1,13 +1,12 @@
 package br.ufsc.ine5410.trade;
 
 import javax.annotation.Nonnull;
+import java.sql.Time;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.*;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantLock;
 
 import static br.ufsc.ine5410.trade.Order.Type.*;
@@ -16,39 +15,44 @@ public class OrderBook implements AutoCloseable {
     private final @Nonnull String stockCode;
     private final @Nonnull TransactionProcessor transactionProcessor;
     //private final @Nonnull PriorityQueue<Order> sellOrders, buyOrders;
-    private final @Nonnull LinkedBlockingQueue<Order> sellO, buyO ;
+    private final @Nonnull LinkedBlockingQueue<Order> sellOrders, buyOrders ;
     private boolean closed = false;
+    private Lock lock;
+
+    private ExecutorService service = Executors.newCachedThreadPool();
 
     public OrderBook(@Nonnull String stockCode,
                      @Nonnull TransactionProcessor transactionProcessor) {
         this.stockCode = stockCode;
         this.transactionProcessor = transactionProcessor;
-        this.sellO =  new LinkedBlockingQueue<>();
-        this.buyO  = new LinkedBlockingQueue<>();
-//        sellOrders = new PriorityQueue<>(100, new Comparator<Order>() {
-//            @Override
-//            public int compare(@Nonnull Order l, @Nonnull Order r) {
-//                return Double.compare(l.getPrice(), r.getPrice());
-//            }
-//        });
+
+        sellOrders =  new LinkedBlockingQueue<>();
+        buyOrders  = new LinkedBlockingQueue<>();
+
+
+        //        sellOrders = new PriorityQueue<>(100, new Comparator<Order>() {
+        //   @Override
+        //   public int compare(@Nonnull Order l, @Nonnull Order r) {
+        //       return Double.compare(l.getPrice(), r.getPrice());
+        //   }
+        //});
+
+        // buyOrders = new PriorityQueue<>(100, new Comparator<Order>() {
+        //  @Override
+        //  public int compare(@Nonnull Order l, @Nonnull Order r) {
+        //      return Double.compare(r.getPrice(), l.getPrice());
+        //  }
+        //});
 
 //        for(Order order : sellOrders) {
 //            sellO.add(order);
 //        }
-
-//        buyOrders = new PriorityQueue<>(100, new Comparator<Order>() {
-//            @Override
-//            public int compare(@Nonnull Order l, @Nonnull Order r) {
-//                return Double.compare(r.getPrice(), l.getPrice());
-//            }
-//        });
-
 //        for(Order order : buyOrders) {
 //            buyO.add(order);
 //        }
     }
 
-    public synchronized void post(@Nonnull Order order) {
+    public synchronized void post(@Nonnull final Order order) {
         if (!order.getStock().equals(stockCode)) {
             String msg = toString() + " cannot process orders for " + order.getStock();
             throw new IllegalArgumentException(msg);
@@ -59,30 +63,30 @@ public class OrderBook implements AutoCloseable {
         }
 
 
-//      (order.getType() == BUY ? buyOrders : sellOrders).add(order);
-
-        (order.getType() == BUY ? buyO : sellO).add(order);
+        (order.getType() == BUY ? buyOrders : sellOrders).add(order);
 
         order.notifyQueued();
-        tryMatch();
+
+        //service.submit(new Runnable() {
+        	//@Override
+            //public void run() {
+            	tryMatch();
+              //}
+            //});
     }
 
-    private void tryMatch() {
+    private void tryMatch() {  // se usar o service.submit acima, colocar synchronized
         Order sell, buy;
 
-        //while ((sell = sellOrders.peek()) != null && (buy = buyOrders.peek()) != null) {
-        while ((sell = sellO.peek()) != null && (buy = buyO.peek()) != null) {
+        while ((sell = sellOrders.peek()) != null && (buy = buyOrders.peek()) != null) {
             if (sell.getPrice() <= buy.getPrice()) {
-                //thread
-                //lock
                 Transaction trans = new Transaction(sell, buy);
-                //Order removed = sellOrders.remove();
-                Order removed = sellO.remove();
+                Order removed = sellOrders.remove();
+                //Order removed = sellO.remove();
                 assert removed == sell;
-                //removed = buyOrders.remove();
-                removed = buyO.remove();
+                removed = buyOrders.remove();
+                //removed = buyO.remove();
                 assert removed == buy;
-                //unlock
 
                 sell.notifyProcessing();
                 buy.notifyProcessing();
@@ -100,18 +104,37 @@ public class OrderBook implements AutoCloseable {
 
     @Override
     public void close()  {
-        if (closed) return;
+        if (closed){
+            return;
+        }
         closed = true;
         //any future post() call will be a no-op
 
-//        for (Order order : sellOrders) order.notifyCancellation();
-//        sellOrders.clear();
-//        for (Order order : buyOrders) order.notifyCancellation();
-//        buyOrders.clear();
+        for (Order order : sellOrders){
+            order.notifyCancellation();
+        }
+        sellOrders.clear();
+        for (Order order : buyOrders){
+            order.notifyCancellation();
+        }
+        buyOrders.clear();
 
-        for (Order order : sellO) order.notifyCancellation();
-        sellO.clear();
-        for (Order order : buyO) order.notifyCancellation();
-        buyO.clear();
+        service.shutdown();
+        try {
+            service.awaitTermination(1, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+//        for (Order order : sellO){
+//            order.notifyCancellation();
+//        }
+//        sellO.clear();
+//
+//        for (Order order : buyO){
+//            order.notifyCancellation();
+//        }
+//        buyO.clear();
     }
 }
